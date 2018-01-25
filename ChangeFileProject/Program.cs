be7a6c00201b9952f45e35070210770bc87fd57e
barbarakat <barbara.accs@gmx.de>
2018-01-25 14:32:15 +0100

    Init

---
+++ ChangeFileProject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ChangeFileProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bye.");
+            //System.Diagnostics.Process process = new System.Diagnostics.Process();
+            //process.EnableRaisingEvents = false;
+            //process.StartInfo.FileName = @"C:\Users\j.gabele.4614\Desktop\test.txt";
+            //process.Start();
+
+            FileStream s = File.OpenWrite(@"C:\Users\j.gabele.4614\Desktop\test.txt");
+            Byte[] info = (new UTF8Encoding(true).GetBytes("Das hier ist ein toller Text."));
+            s.Write(info, 0, info.Length);
+            s.Close();
+            Console.ReadLine();
+        }
+    }
+}
+
